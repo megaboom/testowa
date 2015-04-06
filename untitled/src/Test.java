372ac4b9d035ef430595d0376149292edc33a07a
megaboom <mojprzyjacielubylesmi@gmail.com>
2015-04-06 22:02:29 +0200

    test

---
+++ untitled/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by piasq on 06.04.15.
+ */
+public class Test {
+    public int dom(){
+        return 2*2;
+    }
+    public static void main(String[] args) {
+        Test kolej = new Test();
+        System.out.println(kolej.dom());
+    }
+}
